35a0194459c215a6656d07cc078c2f4566fcf60b
Alexander Kryklia <kryklia@gmail.com>
2017-12-17 12:24:09 +0200

    Fixes for gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   output: {
     path: path.resolve('dist'),
     filename: 'index_bundle.js',
-    publicPath: ''
+    publicPath: '/management-tools/dist'
   },
   module: {
     loaders: [
