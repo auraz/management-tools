4af59c938a19ec9f3a5dfc986de721582973698b
Alexander Kryklia <kryklia@gmail.com>
2017-12-20 22:43:19 +0200

    Added redux for roles page.

--- client/components/roles/Roles.jsx
+++ client/components/roles/Roles.jsx
@@ -1,69 +1,24 @@
 /*
     Render list of roles and for to add new role.
-
-    Q: 1) How to partially update state and rerender? setState is ugly for this (no partial update).
-    Or! model structure maybe too complex for single component?
-
 */
-
 import React from 'react';
+import { connect } from 'react-redux'
 
-
-function addRole(role) {
-        this.state.roles.push(role);
-        localStorage.setItem('roles-state', JSON.stringify(this.state));
-        this.setState(JSON.parse(localStorage.getItem('roles-state')))
-
-        // console.log(role, this.state)
-    }
-
-export default class Roles extends React.Component {
-
-    constructor(props) {
-
-        super(props)
-        addRole = addRole.bind(this);
-
-        this.state = JSON.parse(localStorage.getItem('roles-state')) || {
-            roles: [
-                "Developer",
-                "DevOPS",
-                "QA",
-                "Architect",
-                "Lead",
-                "Product Owner",
-                "Project Coordinator",
-                "Team Coordinator",
-            ],
-
-            grades: [
-            "Junior",
-            "Middle",
-            "Senior",
-            "Lead",
-            ]
-        }
-
-        localStorage.setItem('roles-state', JSON.stringify(this.state)); // needed only in second case
-    }
+class Roles_ extends React.Component {
 
     toRender() {
-
-        this.state = JSON.parse(localStorage.getItem('roles-state'))
-
-        let _roles = _.get(this.state, 'roles', ["Empty list of roles"])
-
-        this.roles = _roles.map((role) => <li key = { role } > {role} </li>);
-
-        return this.roles
+        return this.props.roles.map((role) => <li key = { role } > {role} </li>);
     }
 
     render() {
-        return (
-            <ul>{this.toRender()}</ul>
-            )
+        return (<ul>{this.toRender()}</ul>)
     }
 }
 
+const mapStateToProps = (state) => {
+    return { roles: state.roles}
+}
 
-export { Roles, addRole };
+const Roles = connect(mapStateToProps)(Roles_)
+
+export default Roles
--- client/index.js
+++ client/index.js
@@ -3,12 +3,20 @@
 */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import App from 'components/App.jsx';
-
+import RolesReducer from 'components/roles/reducer.jsx'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'components/common/dashboard.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(RolesReducer);
 
+ReactDOM.render(
+    <Provider store={store}>
+      <App/>
+    </Provider>
+    ,    document.getElementById('root')
+);
