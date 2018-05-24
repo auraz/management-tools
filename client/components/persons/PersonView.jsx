e9974e3ca755adc7cd9989740617f746b63a7408
Alexander Kryklia <kryklia@gmail.com>
2018-05-24 11:55:37 +0300

    Add person progress.

--- client/components/persons/PersonView.jsx
+++ client/components/persons/PersonView.jsx
@@ -3,7 +3,7 @@ import React from "react"
 
 import Row from "../common/Row.jsx"
 import DragAndDropTable from "../common/DragAndDropTable.jsx"
-import { get_skill_name_by_id, get_level_name_by_id, get_param_name_by_id } from "../common/db_helpers.jsx"
+import { get_skill_name_by_id, get_level_name_by_id, get_param_name_by_id, person_health } from "../common/db_helpers.jsx"
 
 import EditablePersonSkillsRow from "./EditablePersonSkillsRow.jsx"
 import EditableParamRow from "../parameters/EditableParamRow.jsx"
@@ -12,6 +12,10 @@ import AddSkillAndAddToPersonForm from "../skills/AddSkillAndAddToPersonForm.jsx
 
 
 class PersonView extends React.Component {
+
+   divStyle(person_id) {
+    return { width: Math.floor(person_health(this.props.persons_skills, person_id) * 100).toFixed(0) + "%" }
+  }
 
   constructor(props) {
     super(props);
@@ -30,7 +34,14 @@ class PersonView extends React.Component {
       <h2>{ this.props.persons.find(el => el.id == this.state.person_id).name }</h2>
       <table>
         <tbody>
-          <tr><th>Strenth</th><th>Weakness</th></tr>
+          <tr><th>Strenth</th><th>Weakness</th><th>
+            <div className="person-progress">
+            Progress
+            <div className="progress">
+             <div className="progress-bar bg-success" style={this.divStyle(this.state.person_id)}></div>
+            </div>
+            </div>
+          </th></tr>
           <tr><td>
             <DragAndDropTable>
             {
@@ -56,10 +67,12 @@ class PersonView extends React.Component {
           }
           </DragAndDropTable>
           <AddParameterAndAddToPersonForm {...this.props} param_table="weaknesses" persons_param_table="persons_weaknesses" person_id={this.state.person_id} />
-          </td></tr>
+          </td>
+          <td>
+          </td>
+          </tr>
         </tbody>
       </table>
-
       <h3>Skills</h3>
       <DragAndDropTable>
       {
@@ -100,7 +113,8 @@ const mapStateToProps = (state) => {
         param_id: key.param_id,
         person_id: key.person_id,
         param_name: get_param_name_by_id(state, key.param_id, "weaknesses"),
-      }))
+      })),
+      persons_skills: state.persons_skills,
     }
 }
 
