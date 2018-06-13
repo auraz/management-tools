3afa46d34122c02430cb4f71cd0480976a681b70
Alexander Kryklia <kryklia@gmail.com>
2018-06-13 17:01:09 +0300

    working postrest.

--- client/components/skills/SkillsTable.jsx
+++ client/components/skills/SkillsTable.jsx
@@ -1,38 +1,47 @@
-import { connect } from 'react-redux'
-import React from "react"
+version: '3.1'
 
-import Row from "../common/Row.jsx"
-import DragAndDropTable from "../common/DragAndDropTable.jsx"
-import EditableRow from "../common/EditableRow.jsx";
-import DeleteControl from "../common/DeleteControl.jsx";
+services:
 
-import { fetchModelAll, fetchPersonsInTeam } from "../common/models"
+  # https://github.com/suzel/docker-postgrest
+  postgrest:
+    build:
+      context: .
+      args:
+        POSTGREST_VERSION: "0.5.0.0"
+    ports:
+      - "3000:3000"
+    environment:
+      PGRST_DB_URI: postgres://app_user:secret@postgres:5432/app_db
+      PGRST_DB_SCHEMA: public
+      PGRST_DB_ANON_ROLE: app_user
+    links:
+      - postgres:postgres
 
+  # https://github.com/sosedoff/pgweb
+  pgweb:
+    image: sosedoff/pgweb
+    ports:
+      - "8081:8081"
+    links:
+      - postgres:postgres
+    environment:
+      - DATABASE_URL=postgres://app_user:secret@postgres:5432/app_db?sslmode=disable
+    depends_on:
+      - postgres
 
-class SkillsTable extends React.Component {
+  # https://hub.docker.com/_/postgres/
+  postgres:
+    image: postgres:alpine
+    ports:
+      - "5432:5432"
+    environment:
+      POSTGRES_DB: app_db
+      POSTGRES_USER: app_user
+      POSTGRES_PASSWORD: secret
+    volumes:
+      - postgres-data:/var/lib/postgresql/data
+      # - ./sql/init.sql:/docker-entrypoint-initdb.d/init.sql
 
-  render() {
-    return <DragAndDropTable>
-    {
-      this.props.skills.map((r) => {
-        return <Row key={r.id} id={r.id}>
-          <th>{r.name}</th>
-          <th>
-            <EditableRow value="" id={r.id} model="skills" formMode="textInput" />
-          </th>
-          <th>
-            <DeleteControl id={r.id} model="skills" /> {/* Should be cascade deletion */}
-          </th>
-        </Row>
-      })
-    }
-    </DragAndDropTable>
-  }
+volumes:
+  postgres-data: {}
 
-}
-
-const mapStateToProps = (state) => {
-   return { skills: state.skills }
-}
-
-export default connect(mapStateToProps)(SkillsTable)
